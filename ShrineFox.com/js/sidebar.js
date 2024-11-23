ee47a51d23fe120255567bb6fd7c92005bddc26b
ShrineFox <ShrineFox@users.noreply.github.com>
2024-11-22 20:22:17 -0500

    Move theme picker to top bar, open in modal

--- ShrineFox.com/js/sidebar.js
+++ ShrineFox.com/js/sidebar.js
@@ -4,6 +4,14 @@
         $('#maincontent').addClass('show');
     });
 
+    $('#theme-toggle').click(function () {
+        $('#theme-modal').addClass('active');
+    });
+
+    $('#theme-modal-close').click(function () {
+        $('#theme-modal').removeClass('active');
+    });
+
     $('.closebtn').click(function () {
         $('#sidebar').removeClass('show');
         $('#maincontent').removeClass('show');
